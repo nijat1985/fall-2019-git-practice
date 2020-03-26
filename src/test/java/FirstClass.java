5972d8a19a670cb3f8911259a7c33488e8a65a34
nijat1985 <mehdiyevnicat1985@gmail.com>
2020-03-25 20:50:28 -0400

    Messing up

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,11 +2,15 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("someone"); //comment
+        System.out.println("brave new world"); // comment
+        System.out.println("someone");
         System.out.println("change one");
         System.out.println("change two");
         System.out.println("change three");
         // first change
         // another change
         // another change
+        System.out.println("working hard");
+        System.out.println("working too much maan");
     }
 }
--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,2 +1,3 @@
 public class SecondClass {
+    //some changes
 }
