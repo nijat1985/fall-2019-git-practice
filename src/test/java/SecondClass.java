329a077ff5172acf33538ccdd92c23d6afd23172
nijat1985 <mehdiyevnicat1985@gmail.com>
2020-04-01 21:15:21 -0400

    ss

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -2,5 +2,6 @@ public class SecondClass {
     //some changes
     public static void main(String[] args) {
         System.out.println("hi every one");
+        System.out.println("hi every one");
     }
 }
